80f4e101867a9cb0d4bc4789c5e5bf26d4125931
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2023-10-04 12:26:25 +0300

    dsa

--- src/App.js
+++ src/App.js
@@ -25,3 +25,11 @@ export default App;
 // Uztaisi divas jaunas komponentets:
 // - Diena.js
 // - Stunda.js
+
+// JS - .forEach() ir masīvu metode,
+// kura apskata ikkatru masīva elementu,
+// bet neko neatgriež (nav return)
+
+// JS - .map() ir masīvu metode,
+// kura apskata ikkatru masīva elementu
+// un atgriež jaunu masīvu (return Array)
